81832e434aa254be5ead47faf96d323eadf62238
andre-pullinen <andre.pullinen@gmail.com>
2020-12-14 11:13:13 +0200

    Add adsense feature

--- js/src/forum/addAdBetweenPosts.js
+++ js/src/forum/addAdBetweenPosts.js
@@ -6,20 +6,18 @@ export default function() {
     extend(PostStream.prototype, 'view', function(component) {
         const advertisement = app.forum.attribute('flagrow.ads.between-posts');
 
-        alert(component.children.length)
-        alert(advertisement)
-
         if (advertisement && component.children.length) {
             const start = parseInt(app.forum.attribute('flagrow.ads.start-from-post') || 1);
             const between = parseInt(app.forum.attribute('flagrow.ads.between-n-posts') || 5);
             // We need to copy all comments first, otherwise there is no way to detect and jump the last comment
             const commentPosts = component.children.filter(post => post.attrs['data-type'] === 'comment');
 
-            alert(commentPosts.length)
-
             // Insert an inside every n comment
             commentPosts.forEach((post, i) => {
                 if (i >= start && (i - start) % between === 0 && i < commentPosts.length - 1) {
+                    alert("push something")
+                    alert(Object.getOwnPropertyNames(post))
+                    alert(Object.getOwnPropertyNames(post.children))
                     post.children.push(
                         m('div.Flagrow-Ads-fake-poststream-item',
                             m('article.Post.EventPost',
