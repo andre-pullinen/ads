2fd2c343af92fa5a5cf1942df67e6d94f3cf7a40
andre-pullinen <andre.pullinen@gmail.com>
2020-12-15 16:16:38 +0200

    Fix ad under header

--- js/src/forum/addAdUnderHeader.js
+++ js/src/forum/addAdUnderHeader.js
@@ -4,14 +4,16 @@ import HeaderPrimary from 'flarum/components/HeaderPrimary';
 export default function () {
     extend(HeaderPrimary.prototype, 'config', (isInitialized, context) => {
 
+        alert(isInitialized)
         if (isInitialized) { return; }
 
+        alert(document.getElementsByClassName('Flagrow-Ads-under-header').length)
         if (document.getElementsByClassName('Flagrow-Ads-under-header').length) {
             return;
         }
 
         const advertisement = app.forum.attribute('flagrow.ads.under-header');
-
+        alert(advertisement)
         if (advertisement) {
 
             var appElement = document.getElementsByClassName('App-content')[0];
